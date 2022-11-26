bab3c4bbc699f4df949e3f909b41ed90f1866aa1
uperbilite <uperbilite@gmail.com>
2022-11-26 22:50:22 +0800

    add book card basic component

---
+++ src/components/BookCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="card" style="width: 18rem">
+      <img
+        class="card-img-top"
+        src="http://www.bingguner.com/upimg/allimg/191210/19-191210103310293.jpg"
+        alt=""
+      />
+      <slot name="bookcover"></slot>
+
+      <div class="card-body">
+        <h5 class="card-title">
+          <slot name="bookname"></slot>
+        </h5>
+        <a href="#" class="btn btn-primary btn-sm">加入购物车</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookCard",
+};
+</script>
+
+<style scoped>
+.card {
+  margin-top: 20px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="home">
+    <BookCard>
+      <template #bookcover></template>
+      <template #bookname>3412</template>
+    </BookCard>
   </div>
 </template>
 
 <script>
+import BookCard from "../components/BookCard.vue";
+
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-  }
-}
+    BookCard,
+  },
+};
 </script>
