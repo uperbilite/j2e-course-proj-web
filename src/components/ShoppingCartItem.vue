b277dec666c9b70da17ae141e4b3772b73f1b1aa
uperbilite <uperbilite@gmail.com>
2022-11-27 01:27:01 +0800

    add shopping cart item component

---
+++ src/components/ShoppingCartItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="card-body">
+        <div class="row align-items-start">
+          <div class="col"><div class="bookname">书名</div></div>
+          <div class="col"><div class="bookprice">价格</div></div>
+          <div class="col">
+            <button type="button" class="btn btn-danger btn-sm">删除</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShoppingCartItem",
+};
+</script>
+
+<style scoped>
+button {
+  float: right;
+}
+.container {
+  margin-top: 20px;
+}
+</style>
