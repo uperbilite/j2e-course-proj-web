88fef924c35c34543ff958a1f18f76e43e1ecf2e
uperbilite <uperbilite@gmail.com>
2022-11-27 00:34:26 +0800

    optimize book card view

--- src/components/BookCard.vue
+++ src/components/BookCard.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="container">
-    <div class="card" style="width: 18rem">
+    <div class="card h-100">
       <img
-        class="card-img-top"
         src="http://www.bingguner.com/upimg/allimg/191210/19-191210103310293.jpg"
+        class="card-img-top"
         alt=""
       />
-      <slot name="bookcover"></slot>
-
       <div class="card-body">
-        <h5 class="card-title">
-          <slot name="bookname"></slot>
-        </h5>
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          This is a longer card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
         <a href="#" class="btn btn-primary btn-sm">加入购物车</a>
       </div>
     </div>
-  </div>
 </template>
 
 <script>
