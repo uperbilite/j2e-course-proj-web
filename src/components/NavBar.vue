55d50e9dc538ccce1ea6afea74b3996a2412d0c9
uperbilite <uperbilite@gmail.com>
2022-11-26 23:57:54 +0800

    reformat code in NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,9 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-      <router-link class="navbar-brand" :to="{name: 'home'}">网上书店</router-link>
+      <router-link class="navbar-brand" :to="{ name: 'home' }"
+        >网上书店</router-link
+      >
       <button
         class="navbar-toggler"
         type="button"
@@ -16,21 +18,34 @@
       <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <router-link class="nav-link active" aria-current="page" :to="{name: 'home'}">主页</router-link>
+            <router-link
+              class="nav-link active"
+              aria-current="page"
+              :to="{ name: 'home' }"
+              >主页</router-link
+            >
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" :to="{name: 'books'}">全部书籍</router-link>
+            <router-link class="nav-link" :to="{ name: 'books' }"
+              >全部书籍</router-link
+            >
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" :to="{name: 'shoppingcart'}">购物车</router-link>
+            <router-link class="nav-link" :to="{ name: 'shoppingcart' }"
+              >购物车</router-link
+            >
           </li>
         </ul>
         <ul class="navbar-nav">
           <li class="nav-item">
-            <router-link class="nav-link" :to="{name: 'login'}">登录</router-link>
+            <router-link class="nav-link" :to="{ name: 'login' }"
+              >登录</router-link
+            >
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" :to="{name: 'register'}">注册</router-link>
+            <router-link class="nav-link" :to="{ name: 'register' }"
+              >注册</router-link
+            >
           </li>
         </ul>
       </div>
