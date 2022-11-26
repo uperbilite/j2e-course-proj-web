2d48588dd0d94e9a76a2c4ae1007c10d2021fb89
uperbilite <uperbilite@gmail.com>
2022-11-26 21:58:04 +0800

    add navigator bar above web page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
+  <NavBar />
   <router-view />
 </template>
 
 <script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap'
+import NavBar from './components/NavBar.vue'
+
 export default {
   name: "App",
   components: {
+    NavBar,
   }
 }
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">网上书店</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarText"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">主页</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">全部书籍</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">登录</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">注册</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style scoped></style>
