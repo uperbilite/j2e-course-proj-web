c46b57e22c37a66839d414244ab9bc408c2134be
uperbilite <uperbilite@gmail.com>
2022-11-27 22:11:31 +0800

    modify login web page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,16 +1,31 @@
 <template>
   <BaseCard>
-    <form>
-      <div class="mb-3">
-        <label for="username" class="form-label">用户名</label>
-        <input v-model="username" type="text" class="form-control" id="username" />
+    <div class="row justify-content-md-center">
+      <div class="col-3">
+        <form>
+          <div class="mb-3">
+            <label for="username" class="form-label">用户名</label>
+            <input
+              v-model="username"
+              type="text"
+              class="form-control"
+              id="username"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="password" class="form-label">密码</label>
+            <input
+              v-model="password"
+              type="password"
+              class="form-control"
+              id="password"
+            />
+          </div>
+          <div class="error-message">{{ error_message }}</div>
+          <button type="submit" class="btn btn-primary">登录</button>
+        </form>
       </div>
-      <div class="mb-3">
-        <label for="password" class="form-label">密码</label>
-        <input v-model="password" type="password" class="form-control" id="password" />
-      </div>
-      <button type="submit" class="btn btn-primary">登录</button>
-    </form>
+    </div>
   </BaseCard>
 </template>
 
@@ -26,13 +41,23 @@ export default {
   setup() {
     let username = ref("");
     let password = ref("");
+    let error_message = ref("");
 
     return {
       username,
       password,
+      error_message,
     };
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+button {
+  width: 100%;
+}
+
+.error-message {
+  color: red;
+}
+</style>
