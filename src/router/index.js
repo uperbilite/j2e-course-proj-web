59a0587d9d25ee15c055fd3335709b42a06bbc8e
uperbilite <uperbilite@gmail.com>
2022-11-27 21:44:40 +0800

    config router of not found page

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,51 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
-import BooksView from '../views/BooksView.vue';
-import ShoppingCartView from '../views/ShoppingCartView.vue';
-import LoginView from '../views/LoginView.vue';
-import RegisterView from '../views/RegisterView.vue';
-import NotFoundView from '../views/NotFoundView.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import BooksView from "../views/BooksView.vue";
+import ShoppingCartView from "../views/ShoppingCartView.vue";
+import LoginView from "../views/LoginView.vue";
+import RegisterView from "../views/RegisterView.vue";
+import NotFoundView from "../views/NotFoundView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/books',
-    name: 'books',
-    component: BooksView
+    path: "/books",
+    name: "books",
+    component: BooksView,
   },
   {
-    path: '/shoppingcart',
-    name: 'shoppingcart',
-    component: ShoppingCartView
+    path: "/shoppingcart",
+    name: "shoppingcart",
+    component: ShoppingCartView,
   },
   {
-    path: '/login',
-    name: 'login',
-    component: LoginView
+    path: "/login",
+    name: "login",
+    component: LoginView,
   },
   {
-    path: '/register',
-    name: 'register',
-    component: RegisterView
+    path: "/register",
+    name: "register",
+    component: RegisterView,
   },
   {
-    path: '/404',
-    name: '404',
-    component: NotFoundView
+    path: "/404",
+    name: "404",
+    component: NotFoundView,
   },
-]
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404",
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
