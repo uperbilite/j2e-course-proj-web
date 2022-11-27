a890f300e2277bea8f4a4ec6f6127771b31a42f0
uperbilite <uperbilite@gmail.com>
2022-11-27 11:17:27 +0800

    complete two component communicate with props and emit

--- src/components/ShoppingCartItem.vue
+++ src/components/ShoppingCartItem.vue
@@ -3,10 +3,20 @@
     <div class="card">
       <div class="card-body">
         <div class="row align-items-start">
-          <div class="col"><div class="bookname">书名</div></div>
-          <div class="col"><div class="bookprice">价格</div></div>
           <div class="col">
-            <button type="button" class="btn btn-danger btn-sm">删除</button>
+            <div class="bookname">书名：{{ book.name }}</div>
+          </div>
+          <div class="col">
+            <div class="bookprice">价格: {{ book.price }}</div>
+          </div>
+          <div class="col">
+            <button
+              @click="deleteItem"
+              type="button"
+              class="btn btn-danger btn-sm"
+            >
+              删除
+            </button>
           </div>
         </div>
       </div>
@@ -17,6 +27,21 @@
 <script>
 export default {
   name: "ShoppingCartItem",
+  props: {
+    book: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup(props, context) {
+    const deleteItem = () => {
+      context.emit("deleteItem");
+    };
+
+    return {
+      deleteItem,
+    };
+  },
 };
 </script>
 
--- src/views/ShoppingCartView.vue
+++ src/views/ShoppingCartView.vue
@@ -1,14 +1,15 @@
 <template>
-  <ShoppingCartItem></ShoppingCartItem>
-  <ShoppingCartItem></ShoppingCartItem>
-  <ShoppingCartItem></ShoppingCartItem>
-  <ShoppingCartItem></ShoppingCartItem>
-  <ShoppingCartItem></ShoppingCartItem>
-  <ShoppingCartItem></ShoppingCartItem>
-  <ShoppingCartItem></ShoppingCartItem>
-  <ShoppingCartItem></ShoppingCartItem>
-  <ShoppingCartItem></ShoppingCartItem>
-  <ShoppingCartItem></ShoppingCartItem>
+  <ShoppingCartItem @deleteItem="deleteItem" :book="book"></ShoppingCartItem>
+  <ShoppingCartItem :book="book"></ShoppingCartItem>
+  <ShoppingCartItem :book="book"></ShoppingCartItem>
+  <ShoppingCartItem :book="book"></ShoppingCartItem>
+  <ShoppingCartItem :book="book"></ShoppingCartItem>
+  <ShoppingCartItem :book="book"></ShoppingCartItem>
+  <ShoppingCartItem :book="book"></ShoppingCartItem>
+  <ShoppingCartItem :book="book"></ShoppingCartItem>
+  <ShoppingCartItem :book="book"></ShoppingCartItem>
+  <ShoppingCartItem :book="book"></ShoppingCartItem>
+
   <nav class="navbar sticky-bottom navbar-light bg-light">
     <div class="container">
       <a class="navbar-brand" href="#">合计：</a>
@@ -18,11 +19,31 @@
 
 <script>
 import ShoppingCartItem from "../components/ShoppingCartItem.vue";
+import { reactive } from "vue";
 
 export default {
   name: "ShoppingCartView",
   components: {
     ShoppingCartItem,
+  },
+  setup() {
+    const book = reactive({
+      id: 100,
+      name: "mybook",
+      cover:
+        "http://www.bingguner.com/upimg/allimg/191210/19-191210103310293.jpg",
+      price: 200,
+      description: "this is a book",
+    });
+
+    const deleteItem = () => {
+      console.log("deleteItem");
+    };
+
+    return {
+      book,
+      deleteItem,
+    };
   },
 };
 </script>
