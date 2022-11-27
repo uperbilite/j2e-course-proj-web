d476fdbb692d6abe7b3cc3d1c1df3dc614b7c749
uperbilite <uperbilite@gmail.com>
2022-11-27 22:04:15 +0800

    bound username and password variable to input

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,11 +3,11 @@
     <form>
       <div class="mb-3">
         <label for="username" class="form-label">用户名</label>
-        <input type="text" class="form-control" id="username" />
+        <input v-model="username" type="text" class="form-control" id="username" />
       </div>
       <div class="mb-3">
         <label for="password" class="form-label">密码</label>
-        <input type="password" class="form-control" id="password" />
+        <input v-model="password" type="password" class="form-control" id="password" />
       </div>
       <button type="submit" class="btn btn-primary">登录</button>
     </form>
@@ -16,11 +16,21 @@
 
 <script>
 import BaseCard from "../components/BaseCard.vue";
+import { ref } from "vue";
 
 export default {
   name: "LoginView",
   components: {
     BaseCard,
+  },
+  setup() {
+    let username = ref("");
+    let password = ref("");
+
+    return {
+      username,
+      password,
+    };
   },
 };
 </script>
