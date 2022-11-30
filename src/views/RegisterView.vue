cb355154d995ee080730a1a3e3afeee844c987f8
uperbilite <uperbilite@gmail.com>
2022-11-30 13:41:00 +0800

    complete register module

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,14 +1,94 @@
 <template>
-  注册
+  <BaseCard>
+    <div class="row justify-content-md-center">
+      <div class="col-3">
+        <form @submit.prevent="register">
+          <div class="mb-3">
+            <label for="username" class="form-label">用户名</label>
+            <input
+              v-model="username"
+              type="text"
+              class="form-control"
+              id="username"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="password" class="form-label">密码</label>
+            <input
+              v-model="password"
+              type="password"
+              class="form-control"
+              id="password"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="confirmed_password" class="form-label">确认密码</label>
+            <input
+              v-model="confirmed_password"
+              type="password"
+              class="form-control"
+              id="confirmed_password"
+            />
+          </div>
+          <div class="error-message">{{ error_message }}</div>
+          <button type="submit" class="btn btn-primary">登录</button>
+        </form>
+      </div>
+    </div>
+  </BaseCard>
 </template>
 
 <script>
+import BaseCard from "../components/BaseCard.vue";
+import { ref } from "vue";
+import $ from "jquery";
 
 export default {
   name: "RegisterView",
   components: {
+    BaseCard,
+  },
+  setup() {
+    let username = ref("");
+    let password = ref("");
+    let confirmed_password = ref("");
+    let error_message = ref("");
+
+    const register = () => {
+      error_message = ref("");
+
+      $.ajax({
+        url: "http://localhost:8081/register",
+        type: "POST",
+        data: JSON.stringify({
+          username: username.value,
+          password: password.value,
+          confirmedPassword: confirmed_password.value,
+        }),
+        contentType: "application/json;charset=utf-8",
+        success(resp) {
+          console.log(resp);
+        },
+      });
+    };
+
+    return {
+      username,
+      password,
+      confirmed_password,
+      error_message,
+      register,
+    };
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+button {
+  width: 100%;
+}
+
+.error-message {
+  color: red;
+}
+</style>
