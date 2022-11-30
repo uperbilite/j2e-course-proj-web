8813ac75eefd873d405c1693a511981dcc43ca39
uperbilite <uperbilite@gmail.com>
2022-11-30 16:22:29 +0800

    set each page a requireAuth, if it is true, visit this page will be needed login first.

--- src/router/index.js
+++ src/router/index.js
@@ -5,37 +5,56 @@ import ShoppingCartView from "../views/ShoppingCartView.vue";
 import LoginView from "../views/LoginView.vue";
 import RegisterView from "../views/RegisterView.vue";
 import NotFoundView from "../views/NotFoundView.vue";
+import store from "@/store";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+    meta: {
+      requireAuth: false,
+    },
   },
   {
     path: "/books",
     name: "books",
     component: BooksView,
+    meta: {
+      requireAuth: false,
+    },
   },
   {
     path: "/shoppingcart/:userId",
     name: "shoppingcart",
     component: ShoppingCartView,
+    meta: {
+      requireAuth: true,
+    },
   },
   {
     path: "/login",
     name: "login",
     component: LoginView,
+    meta: {
+      requireAuth: false,
+    },
   },
   {
     path: "/register",
     name: "register",
     component: RegisterView,
+    meta: {
+      requireAuth: false,
+    },
   },
   {
     path: "/404",
     name: "404",
     component: NotFoundView,
+    meta: {
+      requireAuth: false,
+    },
   },
   {
     path: "/:catchAll(.*)",
@@ -46,6 +65,14 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requireAuth && !store.state.user.is_login) {
+    next({ name: "login" });
+  } else {
+    next();
+  }
 });
 
 export default router;
